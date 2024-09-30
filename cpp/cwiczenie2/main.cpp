630edf9b411d586f8f21bf9bee37aff9e3330971
SzymonSzulik <student@student.machine>
2024-09-30 11:01:35 +0000

    studentmachine send

--- cpp/cwiczenie2/main.cpp
+++ cpp/cwiczenie2/main.cpp
@@ -1,29 +1,47 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
-void Sortowanie(int tab[], int size){
-	for(int i=0; i<size - 1;i++){
-		for(int j=0;j<size - 1;j++){
-			if(tab[j]>tab[j+1])
-				swap(tab[j], tab[j+1]);
-		}
-	}
 
-	for(int y=0;y<4;y++){
-		cout<<tab[y]<<'\n';
-	}
-}
 int main(){
-int tabl[4] = {3,2,5,4};
-int k=tabl[0];
-for(int i=0;i<3;i++){
-	for(int j=0;j<3;j++){
-		if(tabl[j]>tabl[j+1])
-		swap(tabl[j], tabl[j+1]);
-	}
+/////////////////////////////////////////////////////////////////////////////////////////////
+vector<int> numbers;
+
+cout<<"ile liczb?"<<'\n';
+
+int k=0;
+
+cin>>k;
+
+for(int m=0; m<=k-1;m++){
+	cout<<"podaj liczbe"<<'\n';
+	int n=0;
+	cin>>n;
+	numbers.push_back(n);
 }
-for(int y=0;y<4;y++){
-	cout<<tabl[y]<<'\n';
+
+cout<<"Posortowane liczby:"<<'\n';
+
+for(int x=0;x <numbers.size()-1; x++){
+	for(int y=0; y<numbers.size()-1;y++){
+		if(numbers[y]>numbers[y+1]){
+			swap(numbers[y], numbers[y+1]);
+		}
+	} 
 }
+for(int v=0;v<numbers.size();v++){
+cout<<numbers[v]<<'\n';
+}
+
+//int tabl[4] = {3,2,5,4};
+
+// for(int i=0;i<3;i++){
+//	for(int j=0;j<3;j++){
+//		if(tabl[j]>tabl[j+1])
+//		swap(tabl[j], tabl[j+1]);
+//	}
+//}
+//for(int z=0;z<4;z++){
+//	cout<<tabl[z]<<'\n';
+//}
 	return 0;
 }
